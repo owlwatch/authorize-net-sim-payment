9ac8db3af914c462155aef313b4a5f3bf2cf2eef
Mark Fabrizio <fabrizim@owlwatch.com>
2016-02-15 17:15:49 -0500

    Remove sandbox config

---
+++ config.sample.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+  'authorize.net'   => [
+    'transaction_key' => 'Your Authorize.net Transaction Key',
+    'api_login_id'    => 'Your Authorize.net Login Id',
+    'md5_setting'     => 'Your Authorize.net MD5 Hash Setting'
+  ],
+  'title'           => 'Your Site Name',
+  'logo_url'        => 'SSL URL for your logo (recommend Amazon S3)',
+  'site_url'        => 'Your Site\'s URL',
+  'site_link_text'  => 'Site Link in footer',
+  'privacy_url'     => 'Privacy Policy Link'
+];
